509437a5724e7e391c266c722342cb90a0287928
OlegVel <olegv@coins.exchange>
2019-04-23 17:48:08 +0300

    Third iteration

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"os"
 )
 
-func directoryCheck(path string, depth []bool) {
+func directoryCheck(writer io.Writer, path string, depth []bool) {
 	var b bytes.Buffer
 	var prefix string
 	var depthIn []bool
@@ -39,21 +40,24 @@ func directoryCheck(path string, depth []bool) {
 		}
 
 		if file.IsDir() {
-			fmt.Println(b.String() + prefix + file.Name())
-			directoryCheck(path+"/"+file.Name(), depthIn)
+			fmt.Fprintln(writer, b.String()+prefix+file.Name())
+			directoryCheck(writer, path+"/"+file.Name(), depthIn)
 		} else {
-			fmt.Println(b.String() + prefix + file.Name() + " (" + fmt.Sprint(file.Size()) + "b)")
+			fmt.Fprintln(writer, b.String()+prefix+file.Name()+" ("+fmt.Sprint(file.Size())+"b)")
 		}
 	}
 }
 
 func main() {
-	path, err := os.Getwd()
-	if err != nil {
-		logrus.WithError(err)
-		return
-	}
+	writer, _ := os.Create("./path.txt")
+	//path, err := os.Getwd()
+	//if err != nil {
+	//	logrus.WithError(err)
+	//	return
+	//}
+
+	path := "/home/"
 	fmt.Println(path)
 
-	directoryCheck(path, []bool{})
+	directoryCheck(writer, path, []bool{})
 }
